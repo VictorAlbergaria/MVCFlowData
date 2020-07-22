e383fc1a63918775efdac6a5ad551bf363b03e5b
Victor Albergaria <victorspalbergaria@gmail.com>
2020-07-22 12:09:12 -0300

    Flow Data Completo

---
+++ MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLibrary;
+using static DataLibrary.BusinessLogic.EmployeeProcessor;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewEmployees()
+        {
+            ViewBag.Message = "Employees List";
+            var data = LoadEmployees();
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+
+            foreach (var row in data)
+            {
+                employees.Add(new EmployeeModel
+                {
+                    EmplyeeId = row.EmployeeId,
+                    FirstName = row.FirstName,
+                    LastName = row.LastName,
+                    EmailAddress = row.EmailAddress,
+                    ConfirmEmail = row.EmailAddress
+                });
+            }
+
+            return View(employees);
+
+        }
+
+        //Pega os dados do formulário
+        public ActionResult SignUp()
+        {
+            ViewBag.Message = "Emplyee Sign Up.";
+
+            return View();
+        }
+
+        //Envia os dados do formulário
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(EmployeeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                int recordsCreated = CreateEmployee(model.EmplyeeId,
+                    model.FirstName,
+                    model.LastName,
+                    model.EmailAddress);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ MVCDemoDB/dbo/Tables/Employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [EmployeeId] INT NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(100) NOT NULL, 
+    [EmailAddress] NVARCHAR(50) NOT NULL, 
+    
+    
+)
